ab824f7e93dcab40c363c5f70a9b2a568a2a68af
Donia-Kareem <147209045+Donia-Kareem@users.noreply.github.com>
2025-03-02 00:12:14 +0200

    Add files via upload

---
+++ SQLQueryTask A.sql
@@ -0,0 +1,75 @@
+--Create Database
+CREATE DATABASE SocialMedia;
+USE SocialMedia;
+
+--User Table
+CREATE TABLE Users(
+UserID  INT IDENTITY(1,1) PRIMARY KEY,
+UserName VARCHAR(100) NOT NULL,
+DOB DATE NOT NULL,
+email VARCHAR(50) UNIQUE NOT NULL,
+Gender VARCHAR(20) NOT NULL,
+JoinDate DATETIME DEFAULT GETDATE()
+);
+-- Interaction Table 
+CREATE TABLE Interactions(
+InteracID INT IDENTITY(1,1) PRIMARY KEY,
+Type  VARCHAR(50) NOT NULL,
+InteracDate DATETIME DEFAULT GETDATE()
+);
+-- Post Table 
+CREATE TABLE Posts(
+PostID INT IDENTITY(1,1) PRIMARY KEY,
+UserID INT NOT NULL,
+Content TEXT NOT NULL,
+PostDate DATETIME DEFAULT GETDATE(),
+Visibility VARCHAR(20) NOT NULL,
+FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+-- Comments Table 
+CREATE TABLE Comments (
+ CommentID INT IDENTITY(1,1) PRIMARY KEY,
+ UserID INT NOT NULL,
+ PostID INT NOT NULL,
+ CommentDate DATETIME DEFAULT GETDATE(),
+ Content TEXT NOT NULL,
+ FOREIGN KEY (UserID) REFERENCES Users(UserID) ,
+ FOREIGN KEY (PostID) REFERENCES Posts(PostID) 
+);
+-- Interact Table 
+CREATE TABLE Interact (
+ UserID INT NOT NULL,
+ PostID INT NOT NULL,
+ InteractionID INT NOT NULL,
+ PRIMARY KEY (UserID, PostID, InteractionID),
+ FOREIGN KEY (UserID) REFERENCES Users(UserID) ,
+ FOREIGN KEY (PostID) REFERENCES Posts(PostID) ,
+ FOREIGN KEY (InteractionID) REFERENCES Interactions(InteracID) 
+);
+-- Insert users
+INSERT INTO Users(UserName,DOB,email,Gender) VALUES('Donia', '2004-08-03', 'Donia@gmail.com', 'Female'),
+('Kareem', '1998-10-15', 'Kareem@gmail.com', 'Male');
+
+-- Insert posts
+INSERT INTO Posts (UserID, Content, Visibility) VALUES
+(1, 'Hello, this is my first post!', 'Public'),
+(2, 'I Love coffee very much!', 'Friends');
+-- Insert comments
+INSERT INTO Comments (UserID, PostID, Content) VALUES
+(2, 1, 'Nice post'),
+(1, 2, 'Looks delicious!');
+-- Insert interactions
+INSERT INTO Interactions (Type) VALUES
+('Like'), ('Share');
+-- Insert interact
+INSERT INTO Interact (UserID, PostID, InteractionID) VALUES
+(2, 1, 1), 
+(1, 2, 2); 
+
+-----Delete the Users table
+DROP TABLE Users;
+--Drop the entire SocialMedia database.
+DROP DATABASE SocialMedia ;
+
+
+
---
+++ SQLQueryTaskB.sql
@@ -0,0 +1,105 @@
+                        ---Create Database---
+CREATE DATABASE Online_Shopping_System;
+USE Online_Shopping_System;
+                   ---Create Customer Table-------
+CREATE TABLE Customer (
+    CustomerID INT IDENTITY(1,1) PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) UNIQUE NOT NULL,
+    PhoneNumber VARCHAR(20) NOT NULL,
+    Address VARCHAR(255) NOT NULL
+);
+-- Create Order Table
+CREATE TABLE [Order](
+OrderID INT IDENTITY(1,1) PRIMARY KEY,
+CustomerID INT NOT NULL,
+OrderDate DATETIME DEFAULT GETDATE(),
+TotalAmount DECIMAL(10,2) NOT NULL,
+Status VARCHAR NOT NULL,
+FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+);
+--- Create Product Table
+CREATE TABLE Product (
+    ProductID INT IDENTITY(1,1) PRIMARY KEY,
+    Category VARCHAR(100),
+    Name VARCHAR(255) NOT NULL,
+    Description TEXT,
+    Price DECIMAL(10,2) NOT NULL
+);
+--- Create OrderDetails Table
+CREATE TABLE  OrderDetails(
+  OrderDetailsID INT IDENTITY(1,1) PRIMARY KEY,
+  OrderID INT NOT NULL,
+  ProductID INT NOT NULL,
+  Quantity INT NOT NULL,
+  Price DECIMAL(10,2) NOT NULL,
+  FOREIGN KEY (OrderID) REFERENCES [Order](OrderID),
+  FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
+);
+--  CREATE Supplier Table
+CREATE TABLE Supplier (
+    SupplierID INT IDENTITY(1,1) PRIMARY KEY,
+    Name VARCHAR(200) NOT NULL,
+    ContactInfo TEXT
+);
+-- CREATE Product_Supplier Table 
+CREATE TABLE Product_Supplier (
+  SupplierID INT NOT NULL,
+  ProductID INT NOT NULL,
+  PRIMARY KEY (SupplierID, ProductID),
+  FOREIGN KEY (SupplierID) REFERENCES Supplier(SupplierID) ,
+  FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
+);
+----INSERT INTO Customer---
+INSERT INTO Customer (Name, Email, PhoneNumber, Address) 
+VALUES 
+    ('Donia', 'Donia@example.com', '1234567890', '123 SaadEmam Street'),
+    ('Noor', 'Noor@example.com', '0987654321', '456 Abdulaziz Street');
+-----------------------
+ALTER TABLE [Order] ALTER COLUMN Status VARCHAR(20);
+---------------------------
+----Insert into Order Table
+INSERT INTO [Order] (CustomerID, TotalAmount, Status) 
+VALUES (1, 150.75, 'Pending'),
+ (2, 220.50, 'Shipped');
+-- Insert into Product Table
+INSERT INTO Product (Category, Name, Description, Price) 
+VALUES ('Electronics', 'Laptop', 'High-performance laptop ', 10000.00),
+('Clothing', 'T-Shirt', 'Cotton t-shirt', 25.99);
+-- Insert into OrderDetails Table
+INSERT INTO OrderDetails (OrderID, ProductID, Quantity, Price) 
+VALUES (1, 1, 1, 10000.00),  
+ (2, 2, 3, 77.97);    
+ -- Insert into Supplier Table
+ INSERT INTO Supplier (Name, ContactInfo) 
+VALUES ('Join', 'Join@example.com'),
+('ELSA', 'ELSA@example.com');
+
+-- Insert into Product_Supplier Table
+INSERT INTO Product_Supplier (SupplierID, ProductID) 
+VALUES (1, 1), 
+(2, 2); 
+  -----1. DDL
+-----Add a new column named rating to the Products table with an appropriate
+-----data type and a default value of 0.
+ALTER TABLE Product
+ADD rating DECIMAL(3,1) DEFAULT 0;
+---Modify the Category column to have a default value of 'new'.
+ALTER TABLE Product
+ADD CONSTRAINT DF_Product_Category DEFAULT 'new' FOR Category;
+---Drop the rating column from the Products table.
+ALTER TABLE Product
+DROP COLUMN rating;
+    -----------2DML
+-----updatethe order date by setting it to the current date for all records where the order
+--id is greater than 0.
+UPDATE [Order]
+SET OrderDate = GETDATE()
+WHERE OrderID > 0;
+-----Delete all rows from the Products table where the product name is not null
+---and not equal to "Null".
+DELETE FROM Product
+WHERE Name IS NOT NULL 
+AND Name <> 'Null';
+
+
